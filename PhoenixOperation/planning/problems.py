709d14ab426571e048dacf17bb6548af51c6a3d4
Fercho0506 <157646134+Fercho0506@users.noreply.github.com>
2026-05-12 12:19:55 -0500

    Add files via upload

---
+++ PhoenixOperation/planning/problems.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from planning.pddl import Problem
+from planning.domain import DOMAIN
+from world.rescue_layout import RescueLayout
+from world.rescue_rules import build_initial_state
+
+
+class SimpleRescueProblem(Problem):
+    def __init__(self, layout: RescueLayout) -> None:
+        initial_state, objects = build_initial_state(layout)
+
+        ### Your code here ###
+        # El objetivo es que el paciente_0 esté rescatado.
+        goal = frozenset([("Rescued", "patient_0")])
+        ### End of your code ###
+
+        super().__init__(initial_state, goal, DOMAIN, objects)
+        self.layout = layout
+
+
+class MultiRescueProblem(Problem):
+    def __init__(self, layout: RescueLayout) -> None:
+        initial_state, objects = build_initial_state(layout)
+
+        ### Your code here ###
+        # El objetivo es que TODOS los pacientes en la lista 'patients' estén rescatados.
+        goal = frozenset([("Rescued", p) for p in objects["patients"]])
+        ### End of your code ###
+
+        super().__init__(initial_state, goal, DOMAIN, objects)
+        self.layout = layout
